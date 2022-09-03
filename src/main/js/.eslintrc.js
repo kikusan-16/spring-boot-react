3526f136588670127e0d975fdaebbb1a6feb0440
kikusan-16 <ichi6.k1kuch1@gmail.com>
2022-09-03 14:17:51 +0900

    react init

---
+++ src/main/js/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.eslint.json'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    '@typescript-eslint/semi': ['error', 'always'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    'no-void': ['error', { allowAsStatement: true }],
+    '@typescript-eslint/no-unused-vars': [ 'error',
+      {
+        'vars': 'all',
+        'args': 'after-used',
+        'argsIgnorePattern': '_',
+        'ignoreRestSiblings': false,
+        'varsIgnorePattern': '_',
+      },
+    ],
+  }
+}
